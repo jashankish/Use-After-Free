36e76321475bc3d11ca013dc40e9d7477628d4c6
Jashan Kishore <jkishore@umich.edu>
2020-05-07 22:52:51 -0400

    Create standardizedHeader.h

---
+++ standardizedHeader.h
@@ -0,0 +1,20 @@
+// This is a standardized header file that includes most relevant libraries
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <fstream>
+#include <stack>
+#include <map>		
+#include <unordered_map>	
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <cstdlib>
+#include <numeric>	
